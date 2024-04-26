4c022ae1f1ae53f2244470d01eb771951d22373f
isTuann <149751177+isTuann@users.noreply.github.com>
2024-04-26 15:17:15 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,33 @@
 <?php
-$arr=file('images.txt');
-$n=count($arr)-1;
-for ($i=1;$i<=1;$i++){
-$x=rand(0,$n);header("Location:".$arr[$x],"\n");}
-?> 
+
+$filename = "images.txt";
+if(!file_exists($filename)){
+    die('文件不存在');
+}
+ 
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+ 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+ 
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+ 
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+ 
+default:
+    die(header("Location: $pic"));
+}
+?>
