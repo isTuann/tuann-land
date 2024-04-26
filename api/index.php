d29149759af10329183012e9a9a4972099642420
isTuann <149751177+isTuann@users.noreply.github.com>
2024-04-26 14:31:34 +0800

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,34 @@
+<?php
+//存有图片链接的文件名images.txt
+$filename = "images.txt";
+if(!file_exists($filename)){
+    die('指定文件不存在(images.txt)');
+}
+ 
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+ 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+ 
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+ 
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+ 
+default:
+    die(header("Location: $pic"));
+}
+ 
+?>
